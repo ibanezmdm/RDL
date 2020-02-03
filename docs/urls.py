8c584c4211cb80600bdca113c19612a44cc8b81b
Sebastian Cornejo <ibanez.am@gmail.com>
2020-02-02 22:51:57 -0300

    Se generan vistas y templates genericos
    Se genera vistas y templates basicos para index y detalle de reporte

--- docs/urls.py
+++ docs/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'docs'
 urlpatterns = [
-    path('', views.index, name='index'),
+	# ** ex: /docs/
+	path('', views.IndexView.as_view(), name='index'),
+
+	# ** ex: /docs/1/
+	path('<int:pk>/', views.ReporteView.as_view(), name='reporte'),
+
+	# ** ex: /docs/1/pasos
+	path('<int:id_reporte>/pasos/', views.pasos_reporte, name='pasos'),
 ]
--- docs/views.py
+++ docs/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
+
+from .models import Reporte
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+	template_name = 'docs/index.html'
+	context_object_name = 'reporte_list'
+
+	def get_queryset(self):
+		return Reporte.objects.order_by('id')
+
+
+
+class ReporteView(generic.DetailView):
+	model = Reporte
+	template_name = 'docs/reporte.html'
+
+
+
+def pasos_reporte(request, id_reporte):
+  detalle = "Estas buscando los pasos del reporte %s."
+  return HttpResponse(detalle % id_reporte)
