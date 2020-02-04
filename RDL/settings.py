8dbf66b3e270a5145261f7361ca3620e2e789794
Sebastian Cornejo <ibanez.am@gmail.com>
2020-02-03 21:13:10 -0300

    Nueva App AdminLTE
    Se genera nueva app para gestionar templates genericos

--- RDL/settings.py
+++ RDL/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'docs.apps.DocsConfig',
+    'AdminLTE3.apps.Adminlte3Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
